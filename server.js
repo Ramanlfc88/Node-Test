e6aa6478d5af7d7db513dab3cd8429b560268e37
Raman <ramanlfc@gmail.com>
2016-11-16 00:34:35 +0530

    Initial project setup

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+
+function logger(req, res, next) {
+    console.log(new Date()+ ' : '+req.method + ' for route ' + req.originalUrl);
+    next();
+}
+
+app.use(logger);
+
+/*
+app.get('/',function (request, response) {
+    response.send('hello express');
+});
+*/
+app.get('/about', function (req, res, next) {
+   // console.log('about route hit');
+    next();
+}, function (request, response) {
+    response.send('about page');
+});
+
+var publicDir = __dirname + '/public';
+app.use(express.static(publicDir));
+
+app.listen('3000', function () {
+    console.log('server started');
+});
